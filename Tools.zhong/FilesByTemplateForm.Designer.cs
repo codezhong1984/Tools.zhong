16f6fd0ac4ef58a341a41161b0dc825a040dd5cc
superzzq <superzzq@163.com>
2023-09-18 16:34:07 +0800

    功能完善

--- Tools.zhong/FilesByTemplateForm.Designer.cs
+++ Tools.zhong/FilesByTemplateForm.Designer.cs
@@ -45,16 +45,17 @@ namespace Tools.zhong
             this.txtFuncName = new System.Windows.Forms.TextBox();
             this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
             this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnOk
             // 
-            this.btnOk.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnOk.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.btnOk.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.btnOk.Location = new System.Drawing.Point(382, 339);
+            this.btnOk.Location = new System.Drawing.Point(429, 517);
             this.btnOk.Name = "btnOk";
-            this.btnOk.Size = new System.Drawing.Size(100, 33);
+            this.btnOk.Size = new System.Drawing.Size(70, 33);
             this.btnOk.TabIndex = 2;
             this.btnOk.Text = "确 定";
             this.btnOk.UseVisualStyleBackColor = true;
@@ -62,10 +63,10 @@ namespace Tools.zhong
             // 
             // btnCancel
             // 
-            this.btnCancel.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnCancel.Location = new System.Drawing.Point(276, 340);
+            this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCancel.Location = new System.Drawing.Point(353, 517);
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(86, 33);
+            this.btnCancel.Size = new System.Drawing.Size(70, 33);
             this.btnCancel.TabIndex = 3;
             this.btnCancel.Text = "退 出";
             this.btnCancel.UseVisualStyleBackColor = true;
@@ -75,7 +76,7 @@ namespace Tools.zhong
             // 
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(50, 29);
+            this.label6.Location = new System.Drawing.Point(50, 11);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(177, 15);
             this.label6.TabIndex = 7;
@@ -84,7 +85,7 @@ namespace Tools.zhong
             // txtTableName
             // 
             this.txtTableName.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtTableName.Location = new System.Drawing.Point(53, 46);
+            this.txtTableName.Location = new System.Drawing.Point(53, 28);
             this.txtTableName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTableName.MaxLength = 8;
             this.txtTableName.Name = "txtTableName";
@@ -95,7 +96,7 @@ namespace Tools.zhong
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 83);
+            this.label1.Location = new System.Drawing.Point(50, 65);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(191, 15);
             this.label1.TabIndex = 9;
@@ -104,7 +105,7 @@ namespace Tools.zhong
             // txtClassName
             // 
             this.txtClassName.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtClassName.Location = new System.Drawing.Point(53, 100);
+            this.txtClassName.Location = new System.Drawing.Point(53, 82);
             this.txtClassName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtClassName.MaxLength = 8;
             this.txtClassName.Name = "txtClassName";
@@ -115,7 +116,7 @@ namespace Tools.zhong
             // 
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(50, 192);
+            this.label2.Location = new System.Drawing.Point(50, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 15);
             this.label2.TabIndex = 11;
@@ -124,7 +125,7 @@ namespace Tools.zhong
             // txtTemplDir
             // 
             this.txtTemplDir.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtTemplDir.Location = new System.Drawing.Point(53, 209);
+            this.txtTemplDir.Location = new System.Drawing.Point(53, 191);
             this.txtTemplDir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTemplDir.MaxLength = 8;
             this.txtTemplDir.Name = "txtTemplDir";
@@ -134,7 +135,7 @@ namespace Tools.zhong
             // btnTempDir
             // 
             this.btnTempDir.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnTempDir.Location = new System.Drawing.Point(429, 202);
+            this.btnTempDir.Location = new System.Drawing.Point(429, 184);
             this.btnTempDir.Name = "btnTempDir";
             this.btnTempDir.Size = new System.Drawing.Size(53, 33);
             this.btnTempDir.TabIndex = 12;
@@ -145,7 +146,7 @@ namespace Tools.zhong
             // btnOutDir
             // 
             this.btnOutDir.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnOutDir.Location = new System.Drawing.Point(429, 257);
+            this.btnOutDir.Location = new System.Drawing.Point(429, 239);
             this.btnOutDir.Name = "btnOutDir";
             this.btnOutDir.Size = new System.Drawing.Size(53, 33);
             this.btnOutDir.TabIndex = 15;
@@ -157,7 +158,7 @@ namespace Tools.zhong
             // 
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(50, 246);
+            this.label3.Location = new System.Drawing.Point(50, 228);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 15);
             this.label3.TabIndex = 14;
@@ -166,7 +167,7 @@ namespace Tools.zhong
             // txtOutputDir
             // 
             this.txtOutputDir.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtOutputDir.Location = new System.Drawing.Point(53, 263);
+            this.txtOutputDir.Location = new System.Drawing.Point(53, 245);
             this.txtOutputDir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtOutputDir.MaxLength = 8;
             this.txtOutputDir.Name = "txtOutputDir";
@@ -177,7 +178,7 @@ namespace Tools.zhong
             // 
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(50, 139);
+            this.label4.Location = new System.Drawing.Point(50, 121);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 15);
             this.label4.TabIndex = 17;
@@ -186,7 +187,7 @@ namespace Tools.zhong
             // txtFuncName
             // 
             this.txtFuncName.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.txtFuncName.Location = new System.Drawing.Point(53, 156);
+            this.txtFuncName.Location = new System.Drawing.Point(53, 138);
             this.txtFuncName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtFuncName.MaxLength = 8;
             this.txtFuncName.Name = "txtFuncName";
@@ -201,11 +202,24 @@ namespace Tools.zhong
             // 
             this.folderBrowserDialog2.SelectedPath = "E:\\myProjects\\Tools.zhong\\ProjOutput";
             // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Location = new System.Drawing.Point(53, 291);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(429, 189);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "指令列表：%T% ---- 表名\r\n%C% ---- 类名\r\n%LP%--%ELP% 循环 $0--Field0 $Field1....\r\n$0 --- fiel" +
+    "ds\r\n%FDES% ----- 功能描述名指令\r\n%SLP% ---- 循环分隔符指令\r\n%ESLP% ---- 循环分隔符结束指令\r\n%SECTION% -" +
+    "--- 代码段\r\n%END_SECTION% ---- 代码段结束";
+            // 
             // FilesByTemplateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 395);
+            this.ClientSize = new System.Drawing.Size(524, 567);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.txtFuncName);
             this.Controls.Add(this.btnOutDir);
@@ -247,5 +261,6 @@ namespace Tools.zhong
         private System.Windows.Forms.TextBox txtFuncName;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
         private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Label label5;
     }
 }
